09216c26e60f65e630971def47e3b9f6190504cc
Brian Muniz Silveira <155079481+BrianMunizSilveira@users.noreply.github.com>
2024-06-11 10:37:09 -0300

    Aula 06 - Tratamento de Dados

---
+++ exercicios/aula06/script004.js
@@ -0,0 +1,13 @@
+/* var nome = window.prompt('Qual é seu nome?')
+
+document.write(`Olá, <strong>${nome}</strong>! Seu nome tem ${nome.length} letras. <br />`)
+
+document.write(`Seu nome em maiúsculas é ${nome.toUpperCase()} <br />`)
+
+document.write(`Seu nome em minuscúlas é ${nome.toLowerCase()} <br />`) */
+
+var nome = prompt('Qual é o seu nome e sobrenome?')
+
+var idade = prompt('Qual sua idade?')
+
+document.write(`Olá, <strong>${nome}</strong>!. Seu nome tem ${nome.length} letras. <br />E a sua idade é ${idade}`)
