ba666efc089f62ce7b784e225f96bcaa41c98054
Brian Muniz Silveira <155079481+BrianMunizSilveira@users.noreply.github.com>
2024-06-12 11:15:37 -0300

    Exercícios+Desafios

---
+++ desafios/d001.js
@@ -0,0 +1,11 @@
+function botao1(){
+    alert('Você clicou 1° botão!');
+}
+
+function botao2(){
+    alert('Você clicou 2° botão!');
+}
+
+function botao3(){
+    alert('Você clicou 3° botão!');
+}
