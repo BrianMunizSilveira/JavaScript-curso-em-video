a3c308665dcd5445e1366eaa5c156828232979f1
Brian Muniz Silveira <155079481+BrianMunizSilveira@users.noreply.github.com>
2024-07-24 13:23:42 -0300

    Melhorias
    -Melhorias visando a performace, SEO, Acessibilidade e responsividade para a página.
    
    -Melhoria validada pela W3C sem problemas encontrados.

--- exercicios/ex003/script003.js
+++ exercicios/ex003/script003.js
@@ -1,4 +1,4 @@
 function inicio() {
     let nome = window.prompt('Qual é seu nome? ');
-    window.alert(`Olá, ${nome}! É um prazer te conhecer!`);
+    window.alert(`Olá, ${nome}! É um prazer te ver por aqui!`);
 }
