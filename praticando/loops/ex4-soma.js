65b669b396d3677181c2cdc31c561e5beb5b678e
Brian Muniz Silveira <155079481+BrianMunizSilveira@users.noreply.github.com>
2024-08-08 11:33:51 -0300

    Adicionando novos materiais á Index

--- praticando/loops/ex4-soma.js
+++ praticando/loops/ex4-soma.js
@@ -2,7 +2,7 @@ var soma = 0;
 var numero;
 
 do {
-    numero = parseInt(prompt("Digite um numero (0para sair): "), 10);
+    numero = parseInt(prompt("Digite um numero (0 para sair): "), 10);
     soma += numero;
 } while (numero != 0);
 alert(`A soma de todos os números é: ${soma}`);
