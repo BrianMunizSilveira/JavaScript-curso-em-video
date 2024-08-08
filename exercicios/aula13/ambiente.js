f72ffb03df271ff95a9dc72b517306d359aa6fb5
Brian Muniz Silveira <155079481+BrianMunizSilveira@users.noreply.github.com>
2024-08-08 09:48:35 -0300

    Aula 13 – Repetições (Parte 1)

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1; /* Contador*/
+
+do /*Faça isso*/{
+    console.log(`Dia ${c}`);
+    c++ /* Incremento*/ 
+} while /* Enquanto isso*/ (c <= 6);
+
+/*
+var c = 1;
+while (c <= 6) {
+    console.log(`Passo ${c}`);
+    c++
+}
+*/
+
+
+/*
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+*/
