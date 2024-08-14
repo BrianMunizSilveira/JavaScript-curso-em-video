1d845edb2fc9d7904e1c48f937c4bb53b17b0fc2
Brian Muniz Silveira <155079481+BrianMunizSilveira@users.noreply.github.com>
2024-08-14 17:36:41 -0300

    Praticando operadores aritmeticos

---
+++ praticando/calculo-basico/aritmeticos.js
@@ -0,0 +1,30 @@
+/*
+var n1 = 1
+var n2 = 3
+var n3 = 2 
+var n4 = 9
+
+var res =  n1 + n3 ** n3 / n4 
+
+console.log(res); 
+*/
+
+
+let salario = 3480;
+
+let porcentagem = 13;
+
+let valor = (salario * porcentagem) / 100;
+
+let novoSalario = (salario+valor);
+
+console.log('O valor da porcentagem é:', novoSalario);
+
+
+
+// let preço = 1850;
+// let porcentagem = 20;
+// let valor = (preço * porcentagem) / 100;
+// let novoPreço = preço - valor;
+
+// console.log('O valor da porcentagem é:', novoPreço);
