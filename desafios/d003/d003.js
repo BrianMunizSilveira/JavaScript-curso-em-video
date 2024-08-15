f007ee7935b271e6cb03d2312f4c382ab005d8ad
Brian Muniz Silveira <155079481+BrianMunizSilveira@users.noreply.github.com>
2024-08-15 19:00:45 -0300

    Realizando o Desafio 003

---
+++ desafios/d003/d003.js
@@ -0,0 +1,5 @@
+function antesDepois() {
+    let num = Number(prompt ("Olá👋🏽 por favor, digite um número inteiro: "));
+
+    alert(`Você digitou o número ${num}. \nAntes de ${num}, temos o número ${num -1}. \nDepois de ${num}, temos o número ${num +1}. \nQual será o novo número?🤔`);
+}
